e8f2ba2d60e7f66c6a997e172a3fba0dffa13d4d
tkc49 <h.tkc49@gmail.com>
2017-08-17 09:53:31 +0900

    add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+	exit();
+delete_option( 'kintone_to_wp_kintone_field_code_for_post_title' );
+delete_option( 'kintone_to_wp_kintone_field_code_for_terms' );
+delete_option( 'kintone_to_wp_setting_custom_fields' );
+delete_option( 'kintone_to_wp_kintone_url' );
+delete_option( 'kintone_to_wp_kintone_api_token' );	
+delete_option( 'kintone_to_wp_target_appid' );	
+delete_option( 'kintone_to_wp_reflect_post_type' );	
+delete_option( 'kintone_to_wp_kintone_app_form_data' );
